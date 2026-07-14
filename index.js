0f196d0b30fb062ac537de42bdc6e87aa818265b
Tracy Ndanu <kilulutracy@gmail.com>
2026-07-14 23:02:20 +0300

    Completed Lab:Async/await implementation

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your code here!
+document.addEventListener("DOMContentLoaded",() => {
+    const postList = document.getElementById("post-list");
+    async function displayPosts() {
+        try {
+            //Fetch the posts from the API
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+            //convert the response to JSON
+            const posts = await response.json();
+            posts.forEach(post => {
+                const li = document.createElement('li');
+                const h1 =document.createElement('h1');
+                const p = document.createElement('p');
+                h1.textContent = post.title;
+                p.textContent = post.body;
+                li.appendChild(h1);
+                li.appendChild(p);
+                postList.appendChild(li); 
+            });
+    }catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+displayPosts();
+});
